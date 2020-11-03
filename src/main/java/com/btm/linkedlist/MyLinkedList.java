81564b38bc2a4df044976a1de0fe1e8ba54a8262
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-11-03 23:27:37 +0530

    Added:Delete Last Element From The List

---
+++ src/main/java/com/btm/linkedlist/MyLinkedList.java
@@ -0,0 +1,59 @@
+package com.btm.linkedlist;
+
+public class MyLinkedList<K> {
+	public static INode head;
+	public static INode tail;
+
+	public MyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	// UC2
+	public void add(INode<K> newNode) {
+		if (this.tail == null && this.head == null) {
+			this.tail = newNode;
+			this.head = newNode;
+		} else {
+			INode<K> tempNode = this.head;
+			this.head = newNode;
+			this.head.setNext(tempNode);
+		}
+	}
+
+	// UC3
+	public void appendAfter(INode<K> newNode) {
+		if (this.tail == null && this.head == null) {
+			this.tail = newNode;
+			this.head = newNode;
+		} else {
+			this.tail.setNext(newNode);
+			this.tail = newNode;
+		}
+	}
+
+	// UC4
+	public void insertInBetween(INode<K> myNode, INode<K> newNode) {
+		INode<K> tempNode = myNode.getNext();
+		myNode.setNext(newNode);
+		newNode.setNext(tempNode);
+	}
+
+	// UC5
+	public INode<K> delete() {
+		INode<K> tempNode = this.head;
+		this.head = head.getNext();
+		return tempNode;
+	}
+
+	// UC6
+	public INode<K> deleteFromLast() {
+		INode<K> tempNode = this.head;
+		while (!tempNode.getNext().equals(tail)) {
+			tempNode = tempNode.getNext();
+		}
+		this.tail = tempNode;
+		tempNode = tempNode.getNext();
+		return tempNode;
+	}
+}
