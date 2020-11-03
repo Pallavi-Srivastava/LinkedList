b33af86debf89a0d7709698ca575cf41a26309b4
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-11-03 22:09:56 +0530

    Added:Add Element At Last Of The List

---
+++ src/main/java/com/btm/linkedlist/MyLinkedList.java
@@ -0,0 +1,34 @@
+package com.btm.linkedlist;
+
+public class MyLinkedList<K> {
+	public static INode head;
+	public static INode tail;
+
+	public MyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	// UC2
+	public void add(INode<K> newNode) {
+		if (this.tail == null && this.head == null) {
+			this.tail = newNode;
+			this.head = newNode;
+		} else {
+			INode<K> tempNode = this.head;
+			this.head = newNode;
+			this.head.setNext(tempNode);
+		}
+	}
+
+	// UC3
+	public void appendAfter(INode<K> newNode) {
+		if (this.tail == null && this.head == null) {
+			this.tail = newNode;
+			this.head = newNode;
+		} else {
+			this.tail.setNext(newNode);
+			this.tail = newNode;
+		}
+	}
+}
